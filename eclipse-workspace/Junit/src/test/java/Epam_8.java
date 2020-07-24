d8b1c444f073c1747ea8facee5051f2249358c1c
A.V.V.Satyanarayana <alludurgadevi1@gmail.com>
2020-07-24 10:42:43 +0530

    execute the code

---
+++ eclipse-workspace/Junit/src/test/java/Epam_8.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author A.V.V.Satyanarayana
+ *
+ */
+public class Epam_8 {
+		String remove(String string)
+		{
+			String string_after_removig_a = "";
+			int length=string.length();
+			if(length==0)
+			{
+				return "";
+			}
+			else if((length==1 && string.equals("A")) || (length==2 && string.equals("AA")))
+			{
+				return "";
+			}
+			else 
+			{
+				if(string.charAt(0)=='A' && string.charAt(1)=='A')
+				{
+					string_after_removig_a=string.substring(2,length);
+				}
+				else if(string.charAt(0)=='A' && string.charAt(1)!='A')
+				{
+					string_after_removig_a=string.substring(1,length);
+				}
+				else if(string.charAt(0)!='A' && string.charAt(1)=='A')
+				{
+					string_after_removig_a=string.charAt(0)+string.substring(2,length);
+				}
+				else
+				{
+					return string;
+				}
+			}
+			return string_after_removig_a;
+		}
+	}
